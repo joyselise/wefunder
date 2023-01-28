1ac3fe82f593172490f0958b9ce2d279fc6c8f44
Md Rakib Hossain <hrakib435@gmail.com>
2023-01-29 00:06:54 +0600

    hero done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 //Raising slider
+const hero = new Swiper(".hero-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".hero-next",
+        prevEl: ".hero-prev",
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 27,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 27,
+        },
+    },
+});
+
+//Raising slider
 const swiper = new Swiper(".raising-now-slider", {
     // Optional parameters
     direction: "horizontal",
