bd47a72ef63dd245e9dc7c0cb9a579002725af9c
Md Rakib Hossain <hrakib435@gmail.com>
2023-01-29 21:08:17 +0600

    Done

--- js/script.js
+++ js/script.js
@@ -58,3 +58,63 @@ const swiper = new Swiper(".raising-now-slider", {
         },
     },
 });
+
+//blog1 slider
+const blog1 = new Swiper(".blog-slider-1", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".blog1-next",
+        prevEl: ".blog1-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 27,
+        },
+        // when window width is >= 640px
+        769: {
+            slidesPerView: 3,
+            spaceBetween: 27,
+        },
+    },
+});
+
+//blog1 slider
+const blog2 = new Swiper(".blog-slider-2", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".blog2-next",
+        prevEl: ".blog2-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 27,
+        },
+        // when window width is >= 640px
+        769: {
+            slidesPerView: 3,
+            spaceBetween: 27,
+        },
+    },
+});
